30e250be5439bfd182c0a0bf9f5c5f2afc574e7c
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-09-29 16:06:45 -0300

    desafios

---
+++ logica-js-aula_1/logica-js-aula_1/Práticas/app.js
@@ -0,0 +1,28 @@
+{/*
+Desafios
+
+1.Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+2.Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+3.Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+
+4.Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+5.Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.let diaDaSemana = prompt('qual o dia da semana?');
+*/}
+let diaDaSemana = prompt('qual o dia da semana?');
+if (diaDaSemana === "sabado"){
+    alert("Bom fim de semana!");
+}
+else if(diaDaSemana === "domingo"){
+    alert("Bom fim de semana")
+}else{
+    alert("Boa semana!");
+}
+let numero = prompt('Digite um numero para verificar se é negativo ou positivo.')
+if(numero>=0){
+    alert(`${numero} é pósitivo`);
+}else{
+    alert(`${numero} é negativo`)
+}
