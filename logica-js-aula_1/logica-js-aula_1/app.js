ff0e4bee31dc7cf06da6de8efa05cb460e4e713b
lucaslmp2 <lucaspachecolp2@gmail.com>
2025-09-29 14:58:48 -0300

    condicional e concatenação

--- logica-js-aula_1/logica-js-aula_1/app.js
+++ logica-js-aula_1/logica-js-aula_1/app.js
@@ -1,7 +1,13 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
+console.log('Número secreto é:', numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
+console.log('O chute é:', chute);
 
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    console.log(`Isso ai! Você descobriu o número secreto que é:  ${numeroSecreto}`);
+    alert(`Isso ai! Você descobriu o número secreto que é:  ${numeroSecreto}`);
+}else{
+    console.log(`Que pena! O número secreto era:  ${numeroSecreto}`);
+    alert(`Que pena! O número secreto era:  ${numeroSecreto}`);
 }
